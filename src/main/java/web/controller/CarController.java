95fb5480bf89ea293c2bd94f1c55ff8118fedb77
OlegZ <beyondswamps@gmail.com>
2023-04-14 06:32:51 +0300

    Added car count parameter in url

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,9 +1,13 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
+import web.service.CarService;
 import web.service.CarServiceImp;
 
 import java.util.List;
@@ -12,8 +16,8 @@ import java.util.List;
 public class CarController {
     List<Car> cars = new CarServiceImp().listCars(5);
     @GetMapping(value = "/cars")
-    public String printCars(ModelMap model){
-        model.addAttribute("cars", cars);
+    public String printCars(ModelMap model, @RequestParam(defaultValue = "5") Integer count){
+        model.addAttribute("cars", cars.stream().limit(count).toList());
         return "cars";
     }
 }
